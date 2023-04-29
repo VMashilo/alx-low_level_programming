61df773da73e5ddddd8488d2cb86df5f066a0487
VMashilo <valentine.mashulo@gmail.com>
2023-04-29 06:19:27 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*declare variable*/
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		/*print value of ch*/
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+
